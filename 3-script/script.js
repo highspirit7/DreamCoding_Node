48274786419f699546f28c6bb02ad3aa6825ad9b
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-03-16 22:36:01 +0900

    [노드 강의 과제] 파일 정리 스크립트(ver1)

--- 3-script/script.js
+++ 3-script/script.js
@@ -1,27 +1,41 @@
 /**
  * @history
- * 16th Mar 2022(ver0)
+ * 16th Mar 2022(ver0) : 최초에 혼자 시도해서 만듦.
+ * 16th Mar 2022(ver1) : 타겟 폴더명 입력에 문제가 있는 경우 에러 핸들링 처리 추가 및 기타 수정.
  */
+
 // 1. node 명령어에 들어가는 인자 접근하기(폴더명 받기 위해) - process.argv
 
 // 2. 명령어로 받은 폴더명을 가지고 해당 폴더에 접근해 video, captured, duplicated 폴더 생성
 
 // 3. 해당 폴더의 파일을 순회하면서 확장자를 가지고 파일을 분류하고 적절한 폴더로 옮기기
 const { argv } = require('process');
-const fs = require('fs').promises;
+const fs = require('fs');
+const fs_promises = require('fs').promises;
+const os = require('os');
 const path = require('path');
 
-const [currentPath, execFile, targetFolder, ...rest] = argv;
+let [currentPath, execFile, targetFolder, ...rest] = argv;
 
 const folders = ['video', 'captured', 'duplicated'];
 
-fs.readdir(`./${targetFolder}`)
+targetFolder = targetFolder || '';
+const workingDir = path.join(os.homedir(), 'Pictures', targetFolder);
+
+// 타겟 폴더를 아예 입력하지 않거나 입력했는데 존재하지 않는 경로일 경우
+if (!targetFolder || !fs.existsSync(workingDir)) {
+	console.error('Please enter folder name in DREAM_CODING_NODE/3-script!!!');
+	return;
+}
+
+fs_promises
+	.readdir(workingDir)
 	.then((data) => {
 		// const onlyFiles = [];
 
 		for (let newFolder of folders) {
 			if (!data.includes(newFolder)) {
-				fs.mkdir(`./${targetFolder}/${newFolder}`).catch(console.error);
+				fs_promises.mkdir(`${workingDir}/${newFolder}`).catch(console.error);
 			}
 		}
 
@@ -39,26 +53,26 @@ fs.readdir(`./${targetFolder}`)
 		for (let file of files) {
 			const ext = path.extname(file);
 
-			let currentPath = `${__dirname}/${targetFolder}/${file}`;
+			let currentPath = path.join(workingDir, file);
 			let destinationPath = '';
 
 			if (ext === '.mov' || ext === '.mp4') {
-				destinationPath = `${__dirname}/${targetFolder}/video/${file}`;
-				fs.rename(currentPath, destinationPath).catch(console.error);
+				destinationPath = path.join(workingDir, 'video', file);
+				fs_promises.rename(currentPath, destinationPath).catch(console.error);
 			}
 			if (ext === '.png' || ext === '.aae') {
-				destinationPath = `${__dirname}/${targetFolder}/captured/${file}`;
-				fs.rename(currentPath, destinationPath).catch(console.error);
+				destinationPath = path.join(workingDir, 'captured', file);
+				fs_promises.rename(currentPath, destinationPath).catch(console.error);
 			}
 			if (ext === '.jpg') {
 				if (file.includes('E')) {
 					const originalPicName = file.replace('E', '');
 
 					if (files.includes(originalPicName)) {
-						currentPath = `${__dirname}/${targetFolder}/${originalPicName}`;
-						destinationPath = `${__dirname}/${targetFolder}/duplicated/${originalPicName}`;
+						currentPath = path.join(workingDir, originalPicName);
+						destinationPath = path.join(workingDir, 'duplicated', originalPicName);
 
-						fs.rename(currentPath, destinationPath).catch(console.error);
+						fs_promises.rename(currentPath, destinationPath).catch(console.error);
 					}
 				}
 			}
